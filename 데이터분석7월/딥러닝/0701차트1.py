c2bb96c8427c3b35004f7898faab9204582a1d63
narTH <narth8252@gmail.com>
2025-07-21 15:57:41 +0900

    Delete 데이터분석7월/딥러닝 directory

--- 데이터분석7월/딥러닝/0701차트1.py
+++
@@ -1,7 +0,0 @@
-# https://matplotlib.org/stable/tutorials/pyplot.html#sphx-glr-tutorials-pyplot-py
-
-import matplotlib.pyplot as plt
-
-plt.plot([1, 2, 3, 4]) #리스트타입
-plt.ylabel('some numbers')
-plt.show()
